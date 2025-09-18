990aa21843c24d6be15d2c436a2463c34f4e78b8
ahsan.habib01 <ahshapon00@gmail.com>
2025-09-18 23:52:04 +0600

    adding react concepts

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { use } from 'react';
+import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const handleVisitedCountries = country => {
+    // console.log('handle visited countries clicked', country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+
   const countriesData = use(countriesPromise);
-  const countries = countriesData.countries
-  console.log(countries);
+  const countries = countriesData.countries;
+
   return (
     <div>
       <h1>In the countries: {countries.length}</h1>
+      <h2>Total Country Visited: {visitedCountries.length}</h2>
+      <ol>
+        {visitedCountries.map(country => (
+          <li key={country.cca3.cca3}>{country.name.common}</li>
+        ))}
+      </ol>
+      <div className="countries">
+        {countries.map(country => (
+          <Country
+            key={country.cca3.cca3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+          ></Country>
+        ))}
+      </div>
     </div>
   );
 };
